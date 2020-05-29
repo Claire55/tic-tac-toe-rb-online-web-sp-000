d706b4fa2eaf1d9fce74baebe51ed9f23e3fe8cc
Claire Bullock <bullockcm13@gmail.com>
2020-05-29 13:13:59 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,5 +32,8 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+#
 def valid_move?(board, index)
   index.between(0,8) && !position_taken?(board,index)
+end
+
