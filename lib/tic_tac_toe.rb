751965e18242fc34fc9f1e0883b688e7cdcab550
Claire Bullock <bullockcm13@gmail.com>
2020-05-29 13:21:19 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -34,7 +34,11 @@ end
 
 #Checks the index position on board and if the position is nbot taken will return true.
 def valid_move?(board, index)
-  index.between(0,8) && !position_taken?(board,index)
+  if index.between(0,8) && !position_taken?(board,index)
+    return true
+  else 
+    return false 
+  end
 end
 
 def turn(board)
