39fa9b69306340b496a5a582a22377aed36bb49e
Claire Bullock <bullockcm13@gmail.com>
2020-05-29 13:28:44 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -50,5 +50,25 @@ def turn(board)
   end
 end
 
+def turn_count(board)
+  turn = 0 
+  board.each do |index|
+    if index == "X" || index == "O"
+      turn += 1 
+    end
+  end
+  return turn
+end
+
+def current_player(board)
+  num_turns = turn_count(board)
+  if num_turns% 2 == 0 
+    player = "X"
+  else
+    player = "O"
+  end
+  return player
+end
+
 
 
